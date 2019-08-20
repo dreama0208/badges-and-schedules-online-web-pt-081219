d48f34525745ac5f011704b026f907a585379d7e
imperative-genre-4923 <dreama.lin@yale.edu>
2019-08-20 00:07:50 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,25 @@
 # Write your code here.
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array_name)
+    array_name.collect { |x|
+      badge_maker(array_name[x])
+    }
+end
+
+def assign_rooms(speakers)
+  each_with_index = 1
+  speakers.collect { |x|
+    "Hello, speakers[x]! You will be assigned to room #{each_with_index}!"
+    each_with_index += 1
+  }
+end
+
+def printer
+    batch_badge_creator
+    assign_rooms.each { |x|
+    puts assign_rooms[x]
+  }
+end
