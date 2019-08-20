003febeaad082dfd1dd65a41d30c9f3c5eab2c60
imperative-genre-4923 <dreama.lin@yale.edu>
2019-08-20 00:27:14 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -18,8 +18,10 @@ def assign_rooms(speakers)
 end
 
 def printer
-    batch_badge_creator
+    batch_badge_creator.each { |x|
+      puts x
+    }
     assign_rooms.each { |x|
-    puts assign_rooms[x]
+      puts x
   }
 end
